779014c7fdd34f515270475d3c0135979a3d1908
bismo nugroho <bismo.nugroho@cpp.co.id>
2022-10-26 15:03:48 +0700

    update

---
+++ html.h
@@ -0,0 +1,341 @@
+
+static const char PROGMEM INDEX_HTML[] = R"rawliteral(
+<html>
+  <head>
+    <title>Rover Control</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script>
+      let StickStatus={xPosition:0,yPosition:0,x:0,y:0,cardinalDirection:"C"};var JoyStick=function(t,e,i){var o=void 0===(e=e||{}).title?"joystick":e.title,n=void 0===e.width?0:e.width,a=void 0===e.height?0:e.height,r=void 0===e.internalFillColor?"#00AA00":e.internalFillColor,c=void 0===e.internalLineWidth?2:e.internalLineWidth,s=void 0===e.internalStrokeColor?"#003300":e.internalStrokeColor,d=void 0===e.externalLineWidth?2:e.externalLineWidth,u=void 0===e.externalStrokeColor?"#008000":e.externalStrokeColor,h=void 0===e.autoReturnToCenter||e.autoReturnToCenter;i=i||function(t){};var S=document.getElementById(t);S.style.touchAction="none";var f=document.createElement("canvas");f.id=o,0===n&&(n=S.clientWidth),0===a&&(a=S.clientHeight),f.width=n,f.height=a,S.appendChild(f);var l=f.getContext("2d"),k=0,g=2*Math.PI,x=(f.width-(f.width/2+10))/2,v=x+5,P=x+30,m=f.width/2,C=f.height/2,p=f.width/10,y=-1*p,w=f.height/10,L=-1*w,F=m,E=C;function W(){l.beginPath(),l.arc(m,C,P,0,g,!1),l.lineWidth=d,l.strokeStyle=u,l.stroke()}function T(){l.beginPath(),F<x&&(F=v),F+x>f.width&&(F=f.width-v),E<x&&(E=v),E+x>f.height&&(E=f.height-v),l.arc(F,E,x,0,g,!1);var t=l.createRadialGradient(m,C,5,m,C,200);t.addColorStop(0,r),t.addColorStop(1,s),l.fillStyle=t,l.fill(),l.lineWidth=c,l.strokeStyle=s,l.stroke()}function D(){let t="",e=F-m,i=E-C;return i>=L&&i<=w&&(t="C"),i<L&&(t="N"),i>w&&(t="S"),e<y&&("C"===t?t="W":t+="W"),e>p&&("C"===t?t="E":t+="E"),t}"ontouchstart"in document.documentElement?(f.addEventListener("touchstart",function(t){k=1},!1),document.addEventListener("touchmove",function(t){1===k&&t.targetTouches[0].target===f&&(F=t.targetTouches[0].pageX,E=t.targetTouches[0].pageY,"BODY"===f.offsetParent.tagName.toUpperCase()?(F-=f.offsetLeft,E-=f.offsetTop):(F-=f.offsetParent.offsetLeft,E-=f.offsetParent.offsetTop),l.clearRect(0,0,f.width,f.height),W(),T(),StickStatus.xPosition=F,StickStatus.yPosition=E,StickStatus.x=((F-m)/v*100).toFixed(),StickStatus.y=((E-C)/v*100*-1).toFixed(),StickStatus.cardinalDirection=D(),i(StickStatus))},!1),document.addEventListener("touchend",function(t){k=0,h&&(F=m,E=C);l.clearRect(0,0,f.width,f.height),W(),T(),StickStatus.xPosition=F,StickStatus.yPosition=E,StickStatus.x=((F-m)/v*100).toFixed(),StickStatus.y=((E-C)/v*100*-1).toFixed(),StickStatus.cardinalDirection=D(),i(StickStatus)},!1)):(f.addEventListener("mousedown",function(t){k=1},!1),document.addEventListener("mousemove",function(t){1===k&&(F=t.pageX,E=t.pageY,"BODY"===f.offsetParent.tagName.toUpperCase()?(F-=f.offsetLeft,E-=f.offsetTop):(F-=f.offsetParent.offsetLeft,E-=f.offsetParent.offsetTop),l.clearRect(0,0,f.width,f.height),W(),T(),StickStatus.xPosition=F,StickStatus.yPosition=E,StickStatus.x=((F-m)/v*100).toFixed(),StickStatus.y=((E-C)/v*100*-1).toFixed(),StickStatus.cardinalDirection=D(),i(StickStatus))},!1),document.addEventListener("mouseup",function(t){k=0,h&&(F=m,E=C);l.clearRect(0,0,f.width,f.height),W(),T(),StickStatus.xPosition=F,StickStatus.yPosition=E,StickStatus.x=((F-m)/v*100).toFixed(),StickStatus.y=((E-C)/v*100*-1).toFixed(),StickStatus.cardinalDirection=D(),i(StickStatus)},!1)),W(),T(),this.GetWidth=function(){return f.width},this.GetHeight=function(){return f.height},this.GetPosX=function(){return F},this.GetPosY=function(){return E},this.GetX=function(){return((F-m)/v*100).toFixed()},this.GetY=function(){return((E-C)/v*100*-1).toFixed()},this.GetDir=function(){return D()}};
+    </script>
+    <style>
+      body { font-family: Arial; text-align: center; margin:0px auto; padding-top: 30px;}
+      table { margin-left: auto; margin-right: auto; }
+      td { padding: 8 px; }
+      .button {
+        background-color: #2f4468;
+        border: none;
+        color: white;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 18px;
+        margin: 6px 3px;
+        cursor: pointer;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        -webkit-tap-highlight-color: rgba(0,0,0,0);
+      }
+      img {  width: auto ;
+        max-width: 100% ;
+        height: auto ; 
+      }
+      .columnLateral
+{
+  float: left;
+  width: 15%;
+  min-width: 300px;
+}
+.columnCetral
+{
+  float: left;
+  width: 70%;
+  min-width: 300px;
+}
+@media screen and (min-width: 320px) and (max-width: 767px) and (orientation: portrait) {
+  html {
+    transform: rotate(-90deg);
+    transform-origin: left top;
+    width: 100vh;
+    height: 100vw;
+    overflow-x: hidden;
+    position: absolute;
+    top: 100%;
+    left: 0;
+  }
+}
+#joy2Div
+{
+  width:200px;
+  height:200px;
+  margin:50px
+}
+#joystick
+{
+  border: 1px solid #FF0000;
+}
+#joystick2
+{
+  border: 1px solid #0000FF;
+}
+    </style>
+  </head>
+  <body>
+    <h3>Camera</h1>
+    <img src="" id="photo" style="width:640px;height:480px" >
+    <!-- Example of FIXED or ABSOLUTE position -->
+<div style="width: 200px; height: 200px; margin: 30px; position: fixed; top: 40px; right: 1px; touch-action: none;">
+ <div style="
+    right: auto;
+"> Rover Control  
+ </div>
+    <div id="joy3Div" style="width:200px;height:200px;margin:30px;position:fixed;top:40px;right:1px;"></div>
+</div>
+    <div style="position:fixed;bottom:10px;right:1px;">
+      Posizione X:<input id="joy3PosizioneX" type="text" /><br />
+      Posizione Y:<input id="joy3PosizioneY" type="text" /><br />
+      Direzione:<input id="joy3Direzione" type="text" /><br />
+      X :<input id="joy3X" type="text" /></br>
+      Y :<input id="joy3Y" type="text" />
+    </div>
+    
+    <table style="display:none">
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('up');" ontouchstart="toggleCheckbox('up');">Forward</button></td></tr>
+      <tr><td align="center"><button class="button" onmousedown="toggleCheckbox('left');" ontouchstart="toggleCheckbox('left');">Turn Left</button></td><td align="center"></td><td align="center"><button class="button" onmousedown="toggleCheckbox('right');" ontouchstart="toggleCheckbox('right');">Turn Right</button></td></tr>
+      <tr><td colspan="3" align="center"><button class="button" onmousedown="toggleCheckbox('down');" ontouchstart="toggleCheckbox('down');">Backward</button></td></tr>                   
+    </table>
+   <script>
+    function toggleCheckbox(x) {
+     var xhr = new XMLHttpRequest();
+     xhr.open("GET", "/action?go=" + x, true);
+     xhr.send();
+   }
+   
+   
+
+   
+   window.onload = function(){
+    
+    document.getElementById("photo").src = window.location.href.slice(0, -1) + ":81/stream";
+   
+var joy3Param = { "title": "joystick3" };
+//var Joy3 = new JoyStick('joy3Div', joy3Param);
+var lservo1 = 0;
+var lservo2 = 0;
+
+var Joy3 = new JoyStick('joy3Div', 
+{
+    // The ID of canvas element
+    title: 'joystick',
+    // width/height
+    width: undefined,
+    height: undefined,
+    // Internal color of Stick
+    internalFillColor: '#00AA00',
+    // Border width of Stick
+    internalLineWidth: 2,
+    // Border color of Stick
+    internalStrokeColor: '#003300',
+    // External reference circonference width
+    externalLineWidth: 2,
+    //External reference circonference color
+    externalStrokeColor: '#008000',
+    // Sets the behavior of the stick
+    autoReturnToCenter: true
+    
+}, function(stickData) {
+    joy3PosizioneX.value = stickData.xPosition;
+    joy3PosizioneX.value = stickData.yPosition;
+    joy3Direzione.value = stickData.cardinalDirection;
+    joy3X.value = stickData.x;
+    joy3Y.value = stickData.y;
+    
+    request_to_web(stickData);
+});
+
+
+var joy3IinputPosX = document.getElementById("joy3PosizioneX");
+var joy3InputPosY = document.getElementById("joy3PosizioneY");
+var joy3Direzione = document.getElementById("joy3Direzione");
+var joy3X = document.getElementById("joy3X");
+var joy3Y = document.getElementById("joy3Y");
+
+
+
+
+   function request_to_web(stickData){
+   
+     var servo1 = 0;
+     var servo2 = 0;
+     var servox1 = 0;
+     var servox2 = 0;
+     var servo1x = 0;
+     var servo2x = 0;    
+     var step = 3;
+     var mult1 = 1; 
+     var mult2 = 1;
+     
+       
+      if (Joy3.GetY() < 0 ){ 
+        mult1 = -1
+        mult2 = -1;
+      }else{
+        mult1 = 1;
+        mult2 = 1;
+      }
+     // Axiss Y
+     if (Math.abs(stickData.y) <= 20  ){
+
+     
+     }else{
+      servo1 = parseInt(Math.abs(stickData.y) / 20);
+      servo2 = parseInt(Math.abs(stickData.y) / 20) ;
+      remy = Math.abs(stickData.y) % 20;
+      if (remy > 0 ){
+        servo1 = servo1;
+        servo2 = servo2;
+     }
+     }
+      // Axiss X
+      if (Math.abs(stickData.x) <= 20 ){
+     }else{
+      remy = Math.abs(stickData.x) % 20;
+      if (stickData.x > 20 ){
+      //servo1x = Math.abs(parseInt(Joy3.GetX() / 20));
+        servo2x = Math.abs(parseInt(stickData.x/ 20));
+        if (remy > 0){
+          servo2x = servo2x;
+        }
+         if ( Math.abs(stickData.y) <= 20 /* servo2 == 0 */ ){
+           servo1x = ( servo2x * step ) * -1;
+           servo2x = servo1x;
+           mult2 = mult1 * -1;
+         }else{
+           servo2x = parseInt(servo2x / 4 * (servo2 * step));
+         }
+        }
+        else if (stickData.x <= -20){
+
+      //servo1x = parseInt(Joy3.GetX() / 20);
+        servo1x = Math.abs(parseInt(stickData.x / 20));
+        if (remy > 0){
+          servo1x = servo1x ;
+        }
+        
+         if (Math.abs(stickData.y) <= 20  /*servo2 ==0  */){
+             //    console.log("less than 20");
+           servo2x = ( servo1x * step ) * -1;
+           servo1x = servo2x;
+           mult1 =  mult1 * -1;
+          // console.log("servo2x=",servo2x," servo1x",servo1x,"mult=",mult2 );
+         }else{
+           servo1x = parseInt(servo1x / 4 * (servo1 * step));
+         }
+        }
+        
+      }
+      
+     servox1 = 90 + ( ( servo1 * step)   -  servo1x   )      * mult1  ;
+     servox2 = 90 + ( ( servo2 * step)  - servo2x  )   * ( mult2 / -1) ;
+     
+     if (lservo1 != servox1 || lservo2 != servox2) {
+       var xhr = new XMLHttpRequest();
+     xhr.open("GET", "/action?pos1=" + servox1+"&pos2="+servox2, true);
+     xhr.send();
+     
+      lservo1 = servox1;
+      lservo2 = servox2;
+     }
+      
+
+   
+   }
+   
+
+
+
+//setInterval(function(){ //joy3IinputPosX.value=Joy3.GetPosX(); }, 50);
+//setInterval(function(){ //joy3InputPosY.value=Joy3.GetPosY(); }, 50);
+//setInterval(function(){ //joy3Direzione.value=Joy3.GetDir(); }, 50);
+//setInterval(function(){ joy3X.value=Joy3.GetX(); }, 50);
+//setInterval(function(){ joy3Y.value=Joy3.GetY(); }, 50);
+//setInterval(function(){  
+/*
+  
+  var xhr = new XMLHttpRequest();
+     var servo1 = 0;
+     var servo2 = 0;
+     var servox1 = 0;
+     var servox2 = 0;
+     var servo1x = 0;
+     var servo2x = 0;    
+     var step = 3;
+     var mult1 = 1; 
+     var mult2 = 1;
+     
+       
+      if (Joy3.GetY() < 0 ){ 
+        mult1 = -1
+        mult2 = -1;
+      }else{
+        mult1 = 1;
+        mult2 = 1;
+      }
+     // Axiss Y
+     if (Math.abs(Joy3.GetY()) <= 20  ){
+     }else{
+      servo1 = parseInt(Math.abs(Joy3.GetY()) / 20);
+      servo2 = parseInt(Math.abs(Joy3.GetY()) / 20) ;
+      remy = Math.abs(Joy3.GetY()) % 20;
+      if (remy > 0 ){
+        servo1 = servo1;
+        servo2 = servo2;
+     }
+     }
+      // Axiss X
+      if (Math.abs(Joy3.GetX()) <= 20 ){
+     }else{
+      remy = Math.abs(Joy3.GetX()) % 20;
+      if (Joy3.GetX() > 20 ){
+      //servo1x = Math.abs(parseInt(Joy3.GetX() / 20));
+        servo2x = Math.abs(parseInt(Joy3.GetX() / 20));
+        if (remy > 0){
+          servo2x = servo2x;
+        }
+         if (servo2 ==0 ){
+           servo1x = ( servo2x * step ) * -1;
+           servo2x = servo1x;
+           mult2 = mult1 * -1;
+         }else{
+           servo2x = parseInt(servo2x / 4 * (servo2 * step));
+         }
+        }
+        else if (Joy3.GetX() < -20){
+      //servo1x = parseInt(Joy3.GetX() / 20);
+        servo1x = Math.abs(parseInt(Joy3.GetX() / 20));
+        if (remy > 0){
+          servo1x = servo1x ;
+        }
+         if (servo2 ==0 ){
+           servo2x = ( servo1x * step ) * -1;
+           servo1x = servo2x;
+           mult1 =  -1;
+         }else{
+           servo1x = parseInt(servo1x / 4 * (servo1 * step));
+         }
+        }
+        
+      }
+     servox1 = 90 + ( ( servo1 * step)   -  servo1x   )      * mult1  ;
+     servox2 = 90 + ( ( servo2 * step)  - servo2x  )   * ( mult2 / -1) ;
+     
+     if (lservo1 != servox1 || lservo2 != servox2) {
+     xhr.open("GET", "/action?pos1=" + servox1+"&pos2="+servox2, true);
+     xhr.send();
+     
+      lservo1 = servox1;
+      lservo2 = servox2;
+     }
+      
+      */
+//     } , 100000000);
+   }
+  </script>
+  </body>
+</html>
+
+)rawliteral";
---
+++ http.h
@@ -0,0 +1,192 @@
+
+
+#define SERVO_1      12
+#define SERVO_2      13
+
+#define SERVO_STEP   5
+
+Servo servoN1;
+Servo servoN2;
+Servo servo1;
+Servo servo2;
+
+
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+
+int servo1Pos = 90;
+int servo2Pos = 90;
+
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+httpd_handle_t camera_httpd = NULL;
+httpd_handle_t stream_httpd = NULL;
+
+static esp_err_t index_handler(httpd_req_t *req){
+  httpd_resp_set_type(req, "text/html");
+  return httpd_resp_send(req, (const char *)INDEX_HTML, strlen(INDEX_HTML));
+}
+
+static esp_err_t stream_handler(httpd_req_t *req){
+  camera_fb_t * fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t * _jpg_buf = NULL;
+  char * part_buf[64];
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if(res != ESP_OK){
+    return res;
+  }
+
+  while(true){
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    } else {
+      if(fb->width > 400){
+        if(fb->format != PIXFORMAT_JPEG){
+          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+          esp_camera_fb_return(fb);
+          fb = NULL;
+          if(!jpeg_converted){
+            Serial.println("JPEG compression failed");
+            res = ESP_FAIL;
+          }
+        } else {
+          _jpg_buf_len = fb->len;
+          _jpg_buf = fb->buf;
+        }
+      }
+    }
+    if(res == ESP_OK){
+      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+    if(res == ESP_OK){
+      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+    }
+    if(res == ESP_OK){
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+    }
+    if(fb){
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    } else if(_jpg_buf){
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if(res != ESP_OK){
+      break;
+    }
+    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+  }
+  return res;
+}
+
+static esp_err_t cmd_handler(httpd_req_t *req){
+  char*  buf;
+  size_t buf_len;
+  char variable[32] = {0,};
+  char pos1[32] = {0,};
+  char pos2[32] = {0,};
+  
+  buf_len = httpd_req_get_url_query_len(req) + 1;
+  if (buf_len > 1) {
+    buf = (char*)malloc(buf_len);
+    if(!buf){
+      httpd_resp_send_500(req);
+      return ESP_FAIL;
+    }
+    if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+      if (httpd_query_key_value(buf, "pos1", pos1, sizeof(pos1)) == ESP_OK) {
+      } else {
+        free(buf);
+        httpd_resp_send_404(req);
+        return ESP_FAIL;
+      }
+
+      if (httpd_query_key_value(buf, "pos2", pos2, sizeof(pos2)) == ESP_OK) {
+      } else {
+        free(buf);
+        httpd_resp_send_404(req);
+        return ESP_FAIL;
+      }
+      
+    } else {
+      free(buf);
+      httpd_resp_send_404(req);
+      return ESP_FAIL;
+    }
+    free(buf);
+
+  } else {
+    httpd_resp_send_404(req);
+    return ESP_FAIL;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  //flip the camera vertically
+  //s->set_vflip(s, 1);          // 0 = disable , 1 = enable
+  // mirror effect
+  //s->set_hmirror(s, 1);          // 0 = disable , 1 = enable
+
+  int res = 0;
+
+
+  servo1.write( atoi(pos1)); 
+  servo2.write( atoi(pos2)); 
+
+
+        Serial.println("servo1 "+ String(pos1));
+        Serial.println("servo2 "+ String(pos2));
+
+    /*
+  if(!strcmp(variable, "up")) {
+    if(servo1Pos <= 170) {
+      servo1Pos += 5;
+      servo1.write(servo1Pos);
+    }
+    Serial.println(servo1Pos);
+    Serial.println("Up");
+  }
+  else if(!strcmp(variable, "left")) {
+    if(servo2Pos <= 170) {
+      servo2Pos += 5;
+      servo2.write(servo2Pos);
+    }
+    Serial.println(servo2Pos);
+    Serial.println("Left");
+  }
+  else if(!strcmp(variable, "right")) {
+    if(servo2Pos >= 5) {
+      servo2Pos -= 5;
+      servo2.write(servo2Pos);
+    }
+    Serial.println(servo2Pos);
+    Serial.println("Right");
+  }
+  else if(!strcmp(variable, "down")) {
+    if(servo1Pos >= 5) {
+      servo1Pos -= 5;
+      servo1.write(servo1Pos);
+    }
+    Serial.println(servo1Pos);
+    Serial.println("Down");
+  }
+  else {
+    res = -1;
+  }
+  */
+
+  if(res){
+    return httpd_resp_send_500(req);
+  }
+
+  httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+  return httpd_resp_send(req, NULL, 0);
+}
